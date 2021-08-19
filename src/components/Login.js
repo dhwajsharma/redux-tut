208dbc15421afa89f7fcbb36de358013037c0f67
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-19 08:40:38 +0530

    Added login/logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useDispatch } from "react-redux"
+import { login, logout } from '../features/user'
 
 const Login = () => {
+    const dispatch = useDispatch()
+
     return (
         <div>
-            <button>Login</button>
+            <button onClick={() => { dispatch(login({ name: "Dhwaj", age: 20, email: "dhwajsharma10@gmail.com" })) }}>Login</button>
+            <button onClick={() => { dispatch(logout()) }}>Logout</button>
         </div>
     )
 }
